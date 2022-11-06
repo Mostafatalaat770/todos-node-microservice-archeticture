359266f9f2adc99c65d1dd78a2f9cf17fe2dbd52
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-11-07 00:53:27 +0200

    app: serve openapi route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { todosRoute } from './controllers/todos/todo.router';
 import { sendErrorResponse } from './error-handling/error-handler';
+import { serveOpenapiSpec } from './pre-requests-handlers/openapi';
 
 const app = express();
 
 app.use(express.json({ limit: '1mb' }));
 
 app.use('/todos', todosRoute);
+app.use('/openapi.json', serveOpenapiSpec);
 
 app.use(sendErrorResponse);
 export default app;
--- src/pre-requests-handlers/openapi.ts
+++ src/pre-requests-handlers/openapi.ts
@@ -1,7 +1,10 @@
 import { middleware } from 'express-openapi-validator';
 import path from 'path';
+import express from 'express';
 
 const spec = path.join('assets', 'openapi.json');
+
+export const serveOpenapiSpec = express.static(spec);
 
 export const validateInputs = middleware({
     apiSpec: spec,
