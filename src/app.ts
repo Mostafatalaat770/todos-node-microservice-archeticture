c098cd87a7f583e10d8a94ce80e0f802720b40a0
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-11-07 10:39:39 +0200

    app: add swagger endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 import { todosRoute } from './controllers/todos/todo.router';
 import { sendErrorResponse } from './error-handling/error-handler';
+import openapi from '../assets/openapi.json';
 import { serveOpenapiSpec } from './pre-requests-handlers/openapi';
 
 const app = express();
@@ -9,6 +11,8 @@ app.use(express.json({ limit: '1mb' }));
 
 app.use('/todos', todosRoute);
 app.use('/openapi.json', serveOpenapiSpec);
+app.use('/swagger.json', swagger.serve, swagger.setup(openapi));
 
 app.use(sendErrorResponse);
+
 export default app;
