a3162df86fffe03c7ce8e6c5f860c98f9779e7db
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-11-10 21:08:02 +0200

    tests: add mock controller inputs

--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import { sendErrorResponse } from '../error-handling/error-handler';
 import { logRequest } from '../pre-requests-handlers/logger';
 import { validateInputs } from '../pre-requests-handlers/openapi';
@@ -12,3 +12,24 @@ export const server = (configure: (express: Express) => void): Express => {
     app.use(sendErrorResponse);
     return app;
 };
+
+export const mockControllerInputs = (
+    request: Partial<Request>
+): {
+    request: Request;
+    response: Response;
+    next: NextFunction;
+} => ({
+    request: {
+        params: {},
+        ...request,
+    } as unknown as Request,
+    response: {
+        locals: {},
+        status: jest.fn().mockReturnThis(),
+        sendStatus: jest.fn(),
+        send: jest.fn(),
+        end: jest.fn().mockReturnThis(),
+    } as unknown as Response,
+    next: jest.fn() as NextFunction,
+});
