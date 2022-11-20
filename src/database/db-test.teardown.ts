6c7b8c4b72cd8ef0eb8d0159bddaa947b85f1b9f
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-11-20 17:02:54 +0200

    database: destroy connection after tests

--- src/database/db-test.teardown.ts
+++ src/database/db-test.teardown.ts
@@ -1,6 +1,9 @@
+import { db } from './db';
+
 export const teardownDatabase = async (): Promise<void> => {
     // eslint-disable-next-line no-underscore-dangle, @typescript-eslint/no-explicit-any
     (globalThis as any).__DATABASE_CONTAINER__.stop();
+    await db.destroy();
 };
 
 export default teardownDatabase;
