1229f4980c1d1b79133ece3480c74e35829ba86d
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-11-30 00:44:40 +0200

    app: add http error handler middleware
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ import openapi from '../assets/openapi.json';
 import { serveOpenapiSpec } from './pre-requests-handlers/openapi';
 import { logRequest } from './pre-requests-handlers/logger';
 import { healthController } from './controllers/health.controller';
+import { handleHttpError } from './http/http-error-handler';
 
 const app = express();
 
 app.use(express.json({ limit: '1mb' }));
 app.use(logRequest());
 app.use('/todos', todosRoute);
+app.use(handleHttpError);
 app.use('/health', healthController);
 app.use('/openapi.json', serveOpenapiSpec);
 app.use('/swagger.json', swagger.serve, swagger.setup(openapi));
