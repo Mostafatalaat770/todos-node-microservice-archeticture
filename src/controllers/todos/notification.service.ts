0d70e4a71e5d82c7b9ee4eddd0f6fe4507066ae1
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-12-02 00:23:18 +0200

    controllers: todos: add access config to notification service
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/controllers/todos/notification.service.ts
+++ src/controllers/todos/notification.service.ts
@@ -1,7 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { config } from '../../configuration/config';
 
-export const sendNotification = (message: string): Promise<void> =>
-    axios.post(`${config.http.servicesUrl}/notify/notifications`, {
-        message,
-    });
+export const sendNotification = (
+    workspaceId: string,
+    message: string
+): Promise<void> =>
+    axios.post(
+        `${config.http.servicesUrl}/notify/notifications`,
+        {
+            message,
+        },
+        {
+            access: {
+                type: 'workspace',
+                workspaceId,
+            },
+        } as AxiosRequestConfig
+    );
